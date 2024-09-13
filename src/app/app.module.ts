c461815e1b9f15df42afebc8830f622fcb9fc2db
edu <eduardogm81@gmail.com>
2024-09-13 19:00:18 +0200

    Se añaden las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from "./shared/shared.module";
+import { HeroPageComponent } from './heroes/pages/hero-page/hero-page.component';
+import { LayoutPageComponent } from './heroes/pages/layout-page/layout-page.component';
+import { ListPageComponent } from './heroes/pages/list-page/list-page.component';
+import { NewPageComponent } from './heroes/pages/new-page/new-page.component';
+import { SearchPageComponent } from './heroes/pages/search-page/search-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroPageComponent,
+    LayoutPageComponent,
+    ListPageComponent,
+    NewPageComponent,
+    SearchPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
